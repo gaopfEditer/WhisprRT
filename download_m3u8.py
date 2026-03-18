117626f9613dd9cded40509205a6e3c0737712cd
高鹏飞 <gaopengfei@rys.com>
2026-03-18 20:39:03 +0800

    upt

--- download_m3u8.py
+++ download_m3u8.py
@@ -174,28 +174,8 @@ if __name__ == "__main__":
     # 多个 (m3u8_url, output_file) 依次下载
     DOWNLOAD_LIST = [
         (
-            "https://hls.debiqc.cn/videos3/264e47d5e91313566f4ed471e1a28697/264e47d5e91313566f4ed471e1a28697.m3u8?auth_key=1772811114-69aaf36a5c8e7-0-a5f0506f12048a09d80b1000010fd82c&v=3&time=0",
-            "徐婉2.ts",
-        ),
-        (
-            "https://hls.debiqc.cn/videos5/781d3124e3c0eba1a52339844890b29d/781d3124e3c0eba1a52339844890b29d.m3u8?auth_key=1772811637-69aaf575cb6fc-0-612b82cec6c1ac6fca83b89f004362b2&v=3&time=0",
-             "徐婉1.ts",
-        ),
-        (
-            "https://hls.debiqc.cn/videos5/d92af0dc9e6e41d2a7652c49c47b9435/d92af0dc9e6e41d2a7652c49c47b9435.m3u8?auth_key=1772812194-69aaf7a240fe3-0-8ed970bb76f35723b1a1ba39da3fc85f&v=3&time=0", 
-                "big1.ts",
-        ),
-        (
-            "https://hls.debiqc.cn/videos5/d92af0dc9e6e41d2a7652c49c47b9435/d92af0dc9e6e41d2a7652c49c47b9435.m3u8?auth_key=1772812194-69aaf7a240fe3-0-8ed970bb76f35723b1a1ba39da3fc85f&v=3&time=0", 
-                "Shinaryen1.ts",
-        ),
-        (
-            "https://hls.debiqc.cn/videos5/513cc06350ee9edf6b9786f68f49a034/513cc06350ee9edf6b9786f68f49a034.m3u8?auth_key=1772813374-69aafc3edf0df-0-5840e474ba1da69981c7a22d5967516a&v=3&time=0", 
-                "饼干1.ts",
-        ),
-        (
-            "https://hls.debiqc.cn/videos5/75fe55de1f4d31ec0e97e2d2700fd1fa/75fe55de1f4d31ec0e97e2d2700fd1fa.m3u8?auth_key=1772814016-69aafec0e1d87-0-e01a655e0a88c36ba076384aedc10e2e&v=3&time=0", 
-                "娜娜.ts",
+            "https://hls.buejwd.cn/videos5/b7579ca814104a4e0b534054e4e064d5/b7579ca814104a4e0b534054e4e064d5.m3u8?auth_key=1773752728-69b951988aca5-0-d74e9bd3caa9aedd47f221eea7361b99&v=3&time=0",
+            "2222.ts",
         ),
     ]
 
